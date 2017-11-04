32ef347546f74f95c7949799568185e9f7512404
navyhockey56 <navyhockey56@gmail.com>
2017-11-04 13:08:29 -0700

    Create HRintegerComeInAllSizes.py

---
+++ HRintegerComeInAllSizes.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a, b, c, d = int(raw_input()), int(raw_input()), int(raw_input()), int(raw_input())
+print pow(a, b) + pow(c,d)
