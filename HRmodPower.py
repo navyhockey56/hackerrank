35d0e32406c16d5407fc6261204ed09f2f759e3c
navyhockey56 <navyhockey56@gmail.com>
2017-11-04 13:09:30 -0700

    Create HRmodPower.py

---
+++ HRmodPower.py
@@ -0,0 +1,5 @@
+#https://www.hackerrank.com/challenges/python-power-mod-power/problem
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a, b, m = int(raw_input()), int(raw_input()), int(raw_input())
+print pow(a,b)
+print pow(a,b,m)
