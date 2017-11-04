21300cc673c49090f293f54b21b4d2233767dee1
navyhockey56 <navyhockey56@gmail.com>
2017-11-04 12:52:05 -0700

    Create HRevaluation.py

---
+++ HRevaluation.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/python-eval/problem
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import print_function
+eval(raw_input())
