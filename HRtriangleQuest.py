30779c1da7c0a8dfbed7c8f576d606fcbb6d0b5f
navyhockey56 <navyhockey56@gmail.com>
2017-11-04 13:07:53 -0700

    Create HRtriangleQuest.py

---
+++ HRtriangleQuest.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/python-quest-1/problem
+for i in range(1,input()): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print reduce(lambda x, y: x*10 + i, range(i), 0)
+    
